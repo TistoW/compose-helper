eed9481c48e0e784a10fc13ebdcbd1571316b1fc
Inyong Tisto <imtisto@gmail.com>
2026-02-06 13:52:09 +0700

    feat: add core:helper module and update dependencies in build.gradle files

--- core/ui-helper/build.gradle.kts
+++ core/ui-helper/build.gradle.kts
@@ -45,6 +45,7 @@ android {
 dependencies {
 
     implementation(project(":core:data-helper"))
+    implementation(project(":core:helper"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
