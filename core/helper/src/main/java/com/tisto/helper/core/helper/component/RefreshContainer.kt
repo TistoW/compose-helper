6b13572ae51c80d6567fbe8836c98d467f7b648d
Inyong Tisto <imtisto@gmail.com>
2026-03-31 05:36:45 +0700

    feat: update versioning to 1.5.127; add new submodule for compose-helper; modify dependencies in build.gradle.kts

--- core/helper/src/main/java/com/tisto/helper/core/helper/component/RefreshContainer.kt
+++ core/helper/src/main/java/com/tisto/helper/core/helper/component/RefreshContainer.kt
@@ -3,11 +3,13 @@ package com.tisto.helper.core.helper.component
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RefreshContainer(
     modifier: Modifier = Modifier,
