92a9e8a7e8818f06f041694c62a5afd6cbd80fb9
Inyong Tisto <imtisto@gmail.com>
2026-04-06 13:46:03 +0700

    feat: reorganize BaseViewModel and UtilsViewModelType for improved state management and UI event handling

--- core/helper/src/main/java/com/tisto/helper/core/helper/base/BaseViewModel.kt
+++ core/helper/src/main/java/com/tisto/helper/core/helper/base/BaseViewModel.kt
@@ -16,35 +16,6 @@ import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-interface BaseState<R, I, SELF : BaseState<R, I, SELF>> {
-    val items: List<I>
-    val request: R?
-    val item: I?
-
-    fun copies(
-        items: List<I> = this.items,
-        request: R? = this.request,
-        item: I? = this.item
-    ): SELF
-}
-
-sealed interface UiEvent {
-    data class Error(val message: String) : UiEvent
-    data class Success(val message: String) : UiEvent
-    data class Navigate(val route: String) : UiEvent
-    data class Action(val action: String) : UiEvent
-    data class Snackbar(val message: String, val type: SnackbarType = SnackbarType.SUCCESS) :
-        UiEvent
-}
-
-enum class SnackbarType { SUCCESS, ERROR, WARNING, INFO }
-sealed interface UiEffect {
-    data class Toast(
-        val message: String,
-        val type: SnackbarType = SnackbarType.INFO // default value
-    ) : UiEffect
-}
-
 suspend fun SharedFlow<UiEvent>?.collectEvent(
     onError: (String) -> Unit = {},
     onSuccess: (String) -> Unit = {},
@@ -223,31 +194,6 @@ abstract class BaseViewModel<STATE> : ViewModel() {
                 loadedCount = currentItems.size + items.size
             )
         }
-    }
-
-}
-
-class StateHandler<STATE, R, I>(
-    private val getState: () -> STATE?,
-    private val setState: (STATE.() -> STATE) -> Unit
-) where STATE : BaseState<R, I, STATE> {
-
-    fun getItems(): List<I> = getState()?.items ?: emptyList()
-
-    fun getRequest(): R? = getState()?.request
-
-    fun getItem(): I? = getState()?.item
-
-    fun updateItems(update: List<I>.() -> List<I>) {
-        setState { copies(items = items.update()) }
-    }
-
-    fun updateItem(update: I.() -> I?) {
-        setState { copies(item = item?.update()) }
-    }
-
-    fun updateRequest(update: R.() -> R) {
-        setState { copies(request = request?.update()) }
     }
 
 }
